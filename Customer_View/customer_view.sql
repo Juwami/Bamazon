b37603fcd7c35d7ee78663d6f2012537b3778e3e
Juwami <sean.nguyenf@yahoo.com>
2019-07-09 23:39:19 -0400

    added folders for different views

---
+++ Customer_View/customer_view.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT,
+    product_name VARCHAR(50),
+    department_name VARCHAR(50),
+    price DECIMAL(10,2),
+    stock_quantity INT
+);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3027, "Banshee", "France", 134.50, 20)
